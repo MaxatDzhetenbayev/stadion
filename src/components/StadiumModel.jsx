9b26a4b35e86d3fc0e0fb42cd402c0c6524f5636
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2023-10-19 12:43:17 +0600

    change model url

--- src/components/StadiumModel.jsx
+++ src/components/StadiumModel.jsx
@@ -9,7 +9,7 @@ function Model() {
       <Suspense fallback={null}>
         <ModelContent />
       </Suspense>
-		{/* <ModelRotationControl/> */}
+      {/* <ModelRotationControl/> */}
     </Canvas>
   );
 }
