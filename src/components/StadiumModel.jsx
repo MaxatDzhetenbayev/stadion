fedcca8830e6167f0f21346b130b6c8740f4152f
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2023-10-20 09:58:10 +0600

    fix rotate

--- src/components/StadiumModel.jsx
+++ src/components/StadiumModel.jsx
@@ -1,15 +1,16 @@
-import React, { Suspense } from "react";
-import { Canvas, useLoader, useThree, useFrame } from "@react-three/fiber";
+import React from "react";
+import { Canvas, useLoader, useThree } from "@react-three/fiber";
+import { OrbitControls, Stars } from "@react-three/drei";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader";
+
 function Model() {
   return (
-    <Canvas style={{ width: "100vw", height: "100vh", background: "#009052" }}>
-      <ambientLight intensity={1} /> {/* Окружающее освещение */}
+    <Canvas style={{ position: "fixed", top: 0, left: 0,  width: "100vw", height: "100vh", background: "#009052" }}>
+      <ambientLight intensity={1} />
       <directionalLight position={[0, 15, 10]} intensity={7} />
-      <Suspense fallback={null}>
-        <ModelContent />
-      </Suspense>
-      {/* <ModelRotationControl/> */}
+      <OrbitControls enableDamping />
+      <Stars />
+      <ModelContent />
     </Canvas>
   );
 }
@@ -20,25 +21,21 @@ function ModelContent() {
 
   const screenWidth = window.innerWidth;
 
-  //   gltf.scene.scale.set(0.07, 0.07, 0.07);
-  //   gltf.scene.position.set(9, -5, 0);
   gltf.scene.rotation.y = -1.57;
+
 
   if (screenWidth > 920) gltf.scene.scale.set(0.07, 0.07, 0.07);
   if (screenWidth < 920) gltf.scene.scale.set(0.05, 0.05, 0.05);
   if (screenWidth < 720) gltf.scene.scale.set(0.04, 0.04, 0.04);
   if (screenWidth < 460) gltf.scene.scale.set(0.023, 0.023, 0.023);
 
-  // Позиция сцены
   if (screenWidth > 920) gltf.scene.position.set(9, -5, 0);
   if (screenWidth < 920) gltf.scene.position.set(6.9, -5, 0);
   if (screenWidth < 720) gltf.scene.position.set(6.4, -5, 0);
   if (screenWidth < 460) gltf.scene.position.set(3.4, -3, 0);
 
-  useFrame(() => {
-    camera.position.set(0, 7, 10);
-    camera.lookAt(0, 0, 0);
-  });
+
+  camera.position.set(0, 7, 10);
 
   return <primitive object={gltf.scene} />;
 }
