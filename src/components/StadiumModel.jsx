ce867fd252168acb1f1afc9f31b37450bff08149
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2023-10-23 11:40:36 +0600

    add loader

--- src/components/StadiumModel.jsx
+++ src/components/StadiumModel.jsx
@@ -1,28 +1,43 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Canvas, useLoader, useThree } from "@react-three/fiber";
-import { OrbitControls, Stars } from "@react-three/drei";
+import { Loader, OrbitControls, Stars, useGLTF } from "@react-three/drei";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader";
 
 function Model() {
+//   const gltf = useLoader(GLTFLoader, "/untitled.glb", (loader) => {
+//     loader.manager.onLoad = () => {};
+//   });
+
   return (
-    <Canvas style={{ position: "fixed", top: 0, left: 0,  width: "100vw", height: "100vh", background: "#009052" }}>
-      <ambientLight intensity={1} />
-      <directionalLight position={[0, 15, 10]} intensity={7} />
-      <OrbitControls enableDamping />
-      <Stars />
-      <ModelContent />
-    </Canvas>
+    <>
+      <Canvas
+        style={{
+          width: "100vw",
+          height: "100vh",
+          background: "#009052",
+        }}
+      >
+        <ambientLight intensity={1} />
+        <directionalLight position={[0, 15, 10]} intensity={7} />
+        <OrbitControls enableDamping />
+        <Stars />
+        <Suspense fallback={null}>
+          <ModelContent />
+        </Suspense>
+      </Canvas>
+      <Loader />
+    </>
   );
 }
 
 function ModelContent() {
-  const gltf = useLoader(GLTFLoader, "/untitled.glb");
+  const gltf = useGLTF("untitled.glb");
+
   const { camera } = useThree();
 
   const screenWidth = window.innerWidth;
 
   gltf.scene.rotation.y = -1.57;
-
 
   if (screenWidth > 920) gltf.scene.scale.set(0.07, 0.07, 0.07);
   if (screenWidth < 920) gltf.scene.scale.set(0.05, 0.05, 0.05);
@@ -33,7 +48,6 @@ function ModelContent() {
   if (screenWidth < 920) gltf.scene.position.set(6.9, -5, 0);
   if (screenWidth < 720) gltf.scene.position.set(6.4, -5, 0);
   if (screenWidth < 460) gltf.scene.position.set(3.4, -3, 0);
-
 
   camera.position.set(0, 7, 10);
 
